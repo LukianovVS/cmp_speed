1fa745343ec62d99cb5895b3390ee86df450870d
lvs <lukianovvs_work@mail.ru>
2022-12-06 23:03:34 +0300

    работа с целочисленными операциями

---
+++ base_operation/C/main.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include <time.h>
+
+
+#define N_REPEAT 0xFFFFFFFF
+//#define N_REPEAT 0xFFFFF
+
+
+double print_time(char str[], clock_t t_clk, double dt_base)
+{
+    double dt_clk_operation = ((double)t_clk) / N_REPEAT;
+    double dt = dt_clk_operation / CLOCKS_PER_SEC * 1e9;
+    printf("| %20s | %7.3f ns | ", str, dt);
+    if (dt_base < 0.)
+        printf("   -  |\n");
+    else
+        printf(" %4.1f |\n", dt/dt_base);
+    return dt;
+}
+
+
+int sum_int(int a, int b)
+{
+   for(unsigned long i = 0; i < N_REPEAT; i++)
+   {
+       b = a + b;
+   }
+   return b;
+}
+
+
+
+int sub_int(int a, int b)
+{
+    
+   for(unsigned long i = 0; i < N_REPEAT; i++)
+   {
+       b = a - b;
+   }
+   return b;
+}
+
+
+int mul_int(int a, int b)
+{
+   for(unsigned long i = 0; i < N_REPEAT; i++)
+   {
+       b = a * b;
+   }
+   return b;
+}
+
+
+int div_int(int a, int b)
+{
+   for(unsigned long i = 0; i < N_REPEAT; i++)
+   {
+       b = a / b;
+   }
+   return b;
+}
+
+
+
+int div_mod_int(int a, int b)
+{
+    /* addition var for protection against division by zero
+
+    example #1:
+    a = 73
+    b = 5
+    1: 3 = 73 % 5
+    2: 1 = 73 % 3
+    3: 0 = 73 % 1
+    4 error 73 % 0
+    */
+    int c;
+    for(unsigned long i = 0; i < N_REPEAT; i++)
+    {
+        /*
+        if (b == 0)
+        {
+            printf("ERR\n");
+            return;
+        }
+        printf("%d = %d %% %d\n", a % b, a, b);*/
+        c = a % b;// тут проблема. Оптимизация сломает этот цикл
+    }
+    return c;
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    if (argc == 2)
+    {
+        printf("\n### %s\n\n", argv[1]);
+    }
+    clock_t t_clk;
+    double dt_base = -1.;
+    /* в эту переменную будем записывать результаты работ функций
+    * это нужно чтобы при оптимизации код не выключился как "ненужный"
+    * для этого что то надо делать.
+    */
+    int tmp = 0;
+    
+    printf("|      operation       |  time, us  |  coef |\n");
+    printf("|----------------------|------------|-------|\n");
+    
+    t_clk = clock();
+    tmp += sum_int(1, 23);
+    t_clk = clock() - t_clk;
+    dt_base = print_time("sum_int(1, 23)", t_clk, dt_base);
+    
+    t_clk = clock();
+    tmp += sum_int(33, 1232);
+    t_clk = clock() - t_clk;
+    print_time("sum_int(33, 1232)", t_clk, dt_base);
+    
+    
+    t_clk = clock();
+    tmp += sub_int(11, 23);
+    t_clk = clock() - t_clk;
+    print_time("sub_int", t_clk, dt_base);
+    
+    t_clk = clock();
+    tmp += mul_int(3, 7);
+    t_clk = clock() - t_clk;
+    print_time("mul_int", t_clk, dt_base);
+    
+    
+    t_clk = clock();
+    tmp += div_int(7, 3);
+    t_clk = clock() - t_clk;
+    print_time("div_int(7,3)", t_clk, dt_base);
+    
+    t_clk = clock();
+    tmp += div_int(72, 1);
+    t_clk = clock() - t_clk;
+    print_time("div_int(72, 1)", t_clk, dt_base);
+    
+    
+    t_clk = clock();
+    tmp += div_int(64, 2);
+    t_clk = clock() - t_clk;
+    print_time("div_int(64, 2)", t_clk, dt_base);
+    
+    t_clk = clock();
+    tmp += div_mod_int(10, 5); 
+    t_clk = clock() - t_clk;
+    print_time("div_mod_int", t_clk, dt_base);
+    
+    printf("\n");
+    
+    /*в стандартный вывод пищется полезная информация. Мусор отправил в поток ошибок */
+    fprintf(stderr, "rmp val %d\n", tmp);
+    
+    return 0;
+}
---
+++ base_operation/C/winrun.cmd
@@ -0,0 +1,46 @@
+::@echo off
+
+set fout=%~pd0result.md
+set run=%~pd0run.exe 
+
+type nul > %fout%
+
+:: 
+
+call gcc %~pd0main.c -o %run%
+call %run% gcc >> %fout%
+
+
+call g++ %~pd0main.c -o %run%
+call %run% g++ >> %fout%
+
+:: O1
+
+call gcc %~pd0main.c -O -o %run%
+call %run% "gcc -O" >> %fout%
+
+call gcc %~pd0main.c -O1 -o %run%
+call %run% "gcc -O1" >> %fout%
+
+call g++ %~pd0main.c -O -o %run%
+call %run% "g++ -O" >> %fout%
+
+call g++ %~pd0main.c -O1 -o %run%
+call %run% "g++ -O1" >> %fout%
+
+
+:: O2
+
+call gcc %~pd0main.c -O2 -o %run%
+call %run% "gcc -O2" >> %fout%
+
+call g++ %~pd0main.c -O2 -o %run%
+call %run% "g++ -O2" >> %fout%
+
+:: O3
+
+call gcc %~pd0main.c -O3 -o %run%
+call %run% "gcc -O3" >> %fout%
+
+call g++ %~pd0main.c -O3 -o %run%
+call %run% "g++ -O3" >> %fout%
